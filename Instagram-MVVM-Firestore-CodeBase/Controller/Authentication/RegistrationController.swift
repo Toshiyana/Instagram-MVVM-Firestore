5df7ceaa68375a41ec9ee2383ae3859765b3b8f1
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-02-04 15:24:30 +0900

    Add LoginController

---
+++ Instagram-MVVM-Firestore-CodeBase/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,19 @@
+//
+//  RegistrationController.swift
+//  Instagram-MVVM-Firestore-CodeBase
+//
+//  Created by Toshiyana on 2022/02/04.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .yellow
+    }
+
+
+}
