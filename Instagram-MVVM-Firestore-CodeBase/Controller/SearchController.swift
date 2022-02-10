22d99b5c0361d81e617fcca6ed45153207fd9756
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-02-10 18:16:32 +0900

    feat: Add Search Controller UI

--- Instagram-MVVM-Firestore-CodeBase/Controller/SearchController.swift
+++ Instagram-MVVM-Firestore-CodeBase/Controller/SearchController.swift
@@ -7,14 +7,40 @@
 
 import UIKit
 
-class SearchController: UIViewController {
+private let reuseIdentifier = "UserCell"
 
+class SearchController: UITableViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .yellow
+        configureTableView()
     }
 
+    // MARK: - Helpers
+    
+    func configureTableView() {
+        view.backgroundColor = .white
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 64
+    }
 
 }
 
+// MARK: - UITableViewDataSource
+
+extension SearchController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
+}
