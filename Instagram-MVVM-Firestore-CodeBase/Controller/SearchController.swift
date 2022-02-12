2c72f477affa3bd7a649e9e47dbe7ad7031f64a5
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-02-12 16:04:23 +0900

    feat: Search for Users

--- Instagram-MVVM-Firestore-CodeBase/Controller/SearchController.swift
+++ Instagram-MVVM-Firestore-CodeBase/Controller/SearchController.swift
@@ -14,12 +14,19 @@ class SearchController: UITableViewController {
     // MARK: - Properties
     
     private var users = [User]()
+    private var filteredUsers = [User]()
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    private var isSearchMode: Bool {
+        return searchController.isActive && !searchController.searchBar.text!.isEmpty
+    }
     
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureSearchController()
         configureTableView()
         fetchUsers()
     }
@@ -42,18 +49,53 @@ class SearchController: UITableViewController {
         tableView.rowHeight = 64
     }
 
+    func configureSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = false
+    }
 }
 
 // MARK: - UITableViewDataSource
 
 extension SearchController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return isSearchMode ? filteredUsers.count : users.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
-        cell.user = users[indexPath.row]
+        
+        let user = isSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        cell.viewModel = UserCellViewModel(user: user)
         return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension SearchController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let user = isSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        let controller = ProfileController(user: user)
+        navigationController?.pushViewController(controller, animated: true)
+    }
+}
+
+// MARK: - UISearchResultUpdating
+
+extension SearchController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        // this function is called every time that search text was changed
+        guard let searchText = searchController.searchBar.text?.lowercased() else { return }
+        
+        filteredUsers = users.filter({
+            $0.username.contains(searchText) || $0.fullname.lowercased().contains(searchText)
+        })
+        
+        self.tableView.reloadData()
     }
 }
---
+++ Instagram-MVVM-Firestore-CodeBase/ViewModel/UserCellViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  UserCellViewModel.swift
+//  Instagram-MVVM-Firestore-CodeBase
+//
+//  Created by Toshiyana on 2022/02/10.
+//
+
+import Foundation
+
+struct UserCellViewModel {
+    private let user: User
+    
+    var profileImageUrl: URL? {
+        return URL(string: user.profileImageUrl)
+    }
+    
+    var username: String {
+        return user.username
+    }
+    
+    var fullname: String {
+        return user.fullname
+    }
+    
+    init(user: User) {
+        self.user = user
+    }
+}
